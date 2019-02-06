375b2da4d4d64c978e1c450e68af2dcfbfa89c21
m0nstrum <m0nstrum@protonmail.com>
2019-02-06 22:30:57 +0300

    firstver

--- index.js
+++ index.js
@@ -4,9 +4,59 @@ const mongo = require('mongodb')
 module.exports = async function(url, dbname, action, collection, query, newvalue) {
 
 
-
+if(action == 'find'){
     let toret = await MongoClient.connect(url)
-    let dbo = toret.db(dbName);
-    let result = await dbo.collection("perms").find(query).toArray()
+    let dbo = toret.db(dbname);
+    let result = await dbo.collection(collection).find(query).toArray()
+    return result
+}
+else if(action == 'insertOne') {
+   let toret = await MongoClient.connect(url)
+    let dbo = toret.db(dbname);
+	 let result = await dbo.collection(collection).insertOne(query)
+	 return result
+
+}
+else if(action == 'insertMany') {
+	 let toret = await MongoClient.connect(url)
+    let dbo = toret.db(dbname);
+	 let result = await dbo.collection(collection).insertMany(query)
+	 return result
+
+}
+else if(action == 'sort') {
+	 let toret = await MongoClient.connect(url)
+    let dbo = toret.db(dbname);
+	 let result = await dbo.collection(collection).find(query).sort(newvalue).toArray()
+	 return result
+
+}
+else if(action == 'deleteOne') {
+	 let toret = await MongoClient.connect(url)
+    let dbo = toret.db(dbname);
+	 let result = await dbo.collection(collection).deleteOne(query)
+	 return result
+}
+else if(action == 'deleteMany') {
+	 let toret = await MongoClient.connect(url)
+    let dbo = toret.db(dbname);
+	 let result = await dbo.collection(collection).deleteMany(query)
+	 return result
+}
+else if(action == 'updateOne') {
+	 let toret = await MongoClient.connect(url)
+    let dbo = toret.db(dbname);
+	 let result = await dbo.collection(collection).updateOne(query, newvalue)
+	 return result
+}
+else if(action == 'updateMany') {
+	 let toret = await MongoClient.connect(url)
+    let dbo = toret.db(dbname);
+	 let result = await dbo.collection(collection).updateMany(query, newvalue)
+	 return result
+}
+else {
+	return 'unknown action'
+}
 
 }
