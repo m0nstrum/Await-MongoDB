74632de9731953304801ed90e3d8c95362870855
m0nstrum <m0nstrum@yandex.ru>
2020-09-25 11:23:02 +0300

    'd'

--- index.js
+++ index.js
@@ -5,55 +5,70 @@ module.exports = async function(url, dbname, action, collection, query, newvalue
 
 
 if(action == 'find'){
-    let toret = await MongoClient.connect(url)
+    let toret = await MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true })
     let dbo = toret.db(dbname);
     let result = await dbo.collection(collection).find(query).toArray()
-    return result
+	toret.close()
+
+	return result
 }
 else if(action == 'insertOne') {
-   let toret = await MongoClient.connect(url)
+   let toret = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     let dbo = toret.db(dbname);
 	 let result = await dbo.collection(collection).insertOne(query)
-	 return result
+	toret.close()
+
+	return result
 
 }
 else if(action == 'insertMany') {
-	 let toret = await MongoClient.connect(url)
+	 let toret = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     let dbo = toret.db(dbname);
 	 let result = await dbo.collection(collection).insertMany(query)
+	toret.close()
 	 return result
 
 }
 else if(action == 'sort') {
-	 let toret = await MongoClient.connect(url)
+	 let toret = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     let dbo = toret.db(dbname);
 	 let result = await dbo.collection(collection).find(query).sort(newvalue).toArray()
-	 return result
+	toret.close()
+
+	return result
 
 }
 else if(action == 'deleteOne') {
-	 let toret = await MongoClient.connect(url)
+	 let toret = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     let dbo = toret.db(dbname);
 	 let result = await dbo.collection(collection).deleteOne(query)
-	 return result
+	toret.close()
+
+	return result
 }
 else if(action == 'deleteMany') {
-	 let toret = await MongoClient.connect(url)
+	 let toret = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     let dbo = toret.db(dbname);
 	 let result = await dbo.collection(collection).deleteMany(query)
-	 return result
+	toret.close()
+
+	return result
 }
 else if(action == 'updateOne') {
-	 let toret = await MongoClient.connect(url)
+	 let toret = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     let dbo = toret.db(dbname);
 	 let result = await dbo.collection(collection).updateOne(query, newvalue)
-	 return result
+	toret.close()
+
+	return result
 }
 else if(action == 'updateMany') {
-	 let toret = await MongoClient.connect(url)
+	 let toret = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     let dbo = toret.db(dbname);
 	 let result = await dbo.collection(collection).updateMany(query, newvalue)
-	 return result
+	toret.close()
+
+	return result
 }
 else {
 	return 'unknown action'
